60b1318162a1e6958b521fe94e05883456ff33b7
Adam <adam.aljidy@gmail.com>
2021-03-25 10:23:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// Task 1
+
+alert('Hello World!'); 
+console.log('Secret message');
+
+
+// Task 2 
+
+// One line comment
+
+/*  Multi line 
+comment here
+*/
+
+alert("Pop up box message");
+console.log("Message for developers");
+document.write("This in your HTML");
+
+// Task 3 
+
+var greetingMessage = "Hello there!";
+
+alert(greetingMessage);
+console.log(greetingMessage);
+document.write(greetingMessage);
+
+//Task 4 
+
+var numberOfPuppies = 10; 
+var numberOfKittens = 5; 
+var numberOfAnimals = numberOfPuppies + numberOfKittens;
+
+console.log("Number of animals");
+console.log(numberOfAnimals);
+
+var numberOfPeople = 5;
+var numberOfPizzas = 12; 
+
+console.log("Number of pizzas left over");
+console.log(numberOfPizzas % numberOfPeople);
+
+var curentTemp = -5;
+
+console.log("Oppsite of currentTemp");
+console.log(-curentTemp);
+
+//Task 5
+
+var firstName = "Adam";
+var lastName = "Aljidy";
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+var myName = "Adam";
+myName += " Aljidy"
+
+console.log(myName);
